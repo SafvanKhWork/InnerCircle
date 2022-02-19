7a4b1ddb55f1e9e355141c814c5929231367f953
SafvanKhWork <safvankh.lanet@gmail.com>
2022-02-19 13:36:07 +0530

    initiated npm

--- Server/src/index.js
+++ Server/src/index.js
@@ -1,11 +1,13 @@
 const express = require("express");
-// const multer = require("multer");
+const multer = require("multer");
 const path = require("path");
 
 //
+
 require("./db/mongoose");
 
 //
+
 const userRouter = require("./routers/user");
 const productRouter = require("./routers/product");
 const typeRouter = require("./routers/type");
@@ -17,7 +19,7 @@ const Product = require("./models/product");
 const User = require("./models/user");
 const Type = require("./models/type");
 const Circle = require("./models/circle");
-const Recommendation = require("./models/recommendation");
+const Recommendation = require("./models/recommandation");
 
 //
 
@@ -29,8 +31,11 @@ app.listen(port, () => {
 });
 
 app.use(express.json());
-app.use(userRouter);
-app.use(productRouter);
-app.use(typeRouter);
-app.use(circleRouter);
-app.use(recommandationRouter);
+
+//
+
+// app.use(userRouter);
+// app.use(productRouter);
+// app.use(typeRouter);
+// app.use(circleRouter);
+// app.use(recommandationRouter);
